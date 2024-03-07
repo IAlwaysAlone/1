ff37293a5707ac750c60c6af38a20ca5f9e259ca
IAlwaysAlone <zodac010@gmail.com>
2024-03-07 09:13:45 +0300

    jkkjk

---
+++ arra.js
@@ -0,0 +1,9 @@
+function xoroshiechisla(arr){
+    let gydchisela = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0){
+            gydchisela.push(arr[i]);
+        }
+    }
+    return gydchisela;
+}
